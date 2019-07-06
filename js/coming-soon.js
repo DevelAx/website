a436ea764b0c8d0e5110a57b83d912b23e35f4b0
Sergey <13222593+DevelAx@users.noreply.github.com>
2019-07-06 09:08:56 +0300

    Update coming-soon.js

--- js/coming-soon.js
+++ js/coming-soon.js
@@ -3,7 +3,7 @@ var lang = (navigator.language || navigator.browserLanguage).split('-')[0];
 (function($) {
   "use strict"; // Start of use strict
 
-  $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
+  $.getJSON("/data/ru.json", function(a) {
     var html = `<div class="masthead-content text-white py-5 py-md-0">
             <h1 class="mb-3">${a.title}</h1>
             <p class="mb-5">${a.content}</p>
