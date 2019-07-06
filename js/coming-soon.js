9e2a6053421c807bdfb3dff548b4bd07ec8aec46
Sergey <13222593+DevelAx@users.noreply.github.com>
2019-07-06 09:40:36 +0300

    Update coming-soon.js

--- js/coming-soon.js
+++ js/coming-soon.js
@@ -2,20 +2,6 @@
   "use strict"; // Start of use strict
   $(window).bind('hashchange', detectLanguage);
 
-  $.getJSON(`/data/${lang}.json`, function(a) {
-    var html = `<div class="masthead-content text-white py-5 py-md-0">
-            <h1 class="mb-3">${a.title}</h1>
-            <p class="mb-5">${a.content}</p>
-            <div class="input-group input-group-newsletter">
-              <input type="email" class="form-control" placeholder="${a.emailPlaceholder}" aria-label="${a.emailPlaceholder}" aria-describedby="basic-addon">
-              <div class="input-group-append">
-                <button class="btn btn-secondary" type="button">${a.notifyMe}</button>
-              </div>
-            </div>
-          </div>`;
-    $('#content').html(html);
-  });
-  
   function detectLanguage(){
     // detect browser language or url-hash
     var hash = window.location.hash && window.location.hash.substring(1);
@@ -28,6 +14,22 @@
 
     // setup hash-languagge
     window.location.hash = lang;
+  }
+  
+  function updateContent(){
+    $.getJSON(`/data/${lang}.json`, function(a) {
+    var html = `<div class="masthead-content text-white py-5 py-md-0">
+              <h1 class="mb-3">${a.title}</h1>
+              <p class="mb-5">${a.content}</p>
+              <div class="input-group input-group-newsletter">
+                <input type="email" class="form-control" placeholder="${a.emailPlaceholder}" aria-label="${a.emailPlaceholder}" aria-describedby="basic-addon">
+                <div class="input-group-append">
+                  <button class="btn btn-secondary" type="button">${a.notifyMe}</button>
+                </div>
+              </div>
+            </div>`;
+      $('#content').html(html);
+    });
   }
 
 })(jQuery); // End of use strict
