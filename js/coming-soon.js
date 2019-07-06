6e3051c4cb9d8788b8dfa3913a1a9badf8a8f3b0
Sergey <13222593+DevelAx@users.noreply.github.com>
2019-07-06 09:14:50 +0300

    Update coming-soon.js

--- js/coming-soon.js
+++ js/coming-soon.js
@@ -1,9 +1,14 @@
 var lang = (navigator.language || navigator.browserLanguage).split('-')[0];
+var langs = ["en", "ru"];
+
+if (!langs.includes(lang)) {
+  lang = "en"; // default;
+}
 
 (function($) {
   "use strict"; // Start of use strict
 
-  $.getJSON("/data/ru.json", function(a) {
+  $.getJSON(`/data/${lang}.json`, function(a) {
     var html = `<div class="masthead-content text-white py-5 py-md-0">
             <h1 class="mb-3">${a.title}</h1>
             <p class="mb-5">${a.content}</p>
