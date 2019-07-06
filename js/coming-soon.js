80df762b822a067660b6fb189d7991117a5e76c2
Sergey <13222593+DevelAx@users.noreply.github.com>
2019-07-06 09:02:54 +0300

    Update coming-soon.js

--- js/coming-soon.js
+++ js/coming-soon.js
@@ -1,6 +1,22 @@
+var lang = (navigator.language || navigator.browserLanguage).split('-')[0];
+
 (function($) {
   "use strict"; // Start of use strict
 
-  // No JS
+  $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
+    var html = $(`<div class="masthead-content text-white py-5 py-md-0">
+            <h1 class="mb-3">${a.title}</h1>
+            <p class="mb-5">${a.content}</p>
+            <div class="input-group input-group-newsletter">
+              <input type="email" class="form-control" placeholder="{a.emailPlaceholder}" aria-label="${a.emailPlaceholder}" aria-describedby="basic-addon">
+              <div class="input-group-append">
+                <button class="btn btn-secondary" type="button">${a.notifyMe}</button>
+              </div>
+            </div>
+          </div>`);
+    $('#quotes').append(quoteHTML);
+  });
+  
+
 
 })(jQuery); // End of use strict
