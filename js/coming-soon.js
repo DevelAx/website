e0b376f704c5e6f30c3851ae456f307320c8407c
Sergey <13222593+DevelAx@users.noreply.github.com>
2019-07-06 09:09:52 +0300

    Update coming-soon.js

--- js/coming-soon.js
+++ js/coming-soon.js
@@ -8,7 +8,7 @@ var lang = (navigator.language || navigator.browserLanguage).split('-')[0];
             <h1 class="mb-3">${a.title}</h1>
             <p class="mb-5">${a.content}</p>
             <div class="input-group input-group-newsletter">
-              <input type="email" class="form-control" placeholder="{a.emailPlaceholder}" aria-label="${a.emailPlaceholder}" aria-describedby="basic-addon">
+              <input type="email" class="form-control" placeholder="${a.emailPlaceholder}" aria-label="${a.emailPlaceholder}" aria-describedby="basic-addon">
               <div class="input-group-append">
                 <button class="btn btn-secondary" type="button">${a.notifyMe}</button>
               </div>
